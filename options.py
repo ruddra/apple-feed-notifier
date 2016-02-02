1044f3881114bfe3bb705e8fbf66dabc24b26108
Arnab Kumar Shil <arnab.kumar@iappdragon.com>
2016-02-02 16:13:20 +0600

    updated code

--- options.py
+++
@@ -1,9 +0,0 @@
-RSS_URL = 'http://stackoverflow.com/feeds/tag?tagnames=django&sort=newest'
-RSS_TITLE = "Django Latest"
-NOTIFICATION_DELAY = 5  # seconds
-CHECK_DELAY = 60  # seconds
-
-try:
-    from _options import *
-except ImportError:
-    print('No custom configuration assigned')
--- run.py
+++ run.py
@@ -1,19 +1,30 @@
 import feedparser
 import subprocess
-# import Foundation
 import time
+import sys
 
-from options import RSS_URL, RSS_TITLE, NOTIFICATION_DELAY, CHECK_DELAY
+notification_delay = 5
+check_delay = 60
+title = "Ruddra Website"
+rss = 'https://feedity.com/ruddra-com/VFZQWlFU.rss'
 
+if '-nt' in sys.argv:
+    notification_delay = int(sys.argv[sys.argv.index('-nt') + 1])
 
-rss_url = RSS_URL
-rss_title = RSS_TITLE
+if '-ct' in sys.argv:
+    check_delay = int(sys.argv[sys.argv.index('-ct') + 1])
+
+if '-t' in sys.argv:
+    title = sys.argv[sys.argv.index('-t') + 1]
+
+if '-u' in sys.argv:
+    rss = sys.argv[sys.argv.index('-u') + 1]
 
 last_updated_time = None
 dup_ids = list()
 apple_cmd = "osascript -e '{0}'"
 while True:
-    d = feedparser.parse(rss_url)
+    d = feedparser.parse(rss)
     updated_time = d.feed.updated
     if updated_time == last_updated_time:
         print('No new feed')
@@ -28,10 +39,10 @@ while True:
             else:
                 print(_notification)
                 dup_ids.append(_id)
-                base_cmd = 'display notification "{0}" with title "{1}"'.format(_notification, rss_title)
+                base_cmd = 'display notification "{0}" with title "{1}"'.format(_notification, title)
                 cmd = apple_cmd.format(base_cmd)
                 subprocess.Popen([cmd], shell=True)
-                time.sleep(NOTIFICATION_DELAY)
-    time.sleep(CHECK_DELAY)
+                time.sleep(notification_delay)
+    time.sleep(check_delay)
 
-
+# python run.py -nt 5 -ct 10 -t DjangoLatest -u http://stackoverflow.com/feeds/tag\?tagnames\=django\&sort\=newest
